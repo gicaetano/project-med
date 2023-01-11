5bf1643327ead9302019b01ee0eda32151b96460
Your Name <you@example.com>
2023-01-11 18:51:13 -0300

    api medico em desenv

---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,33 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "medicos")
+@Entity(name = "Medico")
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Medico(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.crm = dados.crm();
+        this.endereco = new Endereco(dados.endereco());
+    }
+}
